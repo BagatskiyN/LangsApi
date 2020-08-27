81955910908a22d08116ffcec7dd4974c0052e2a
Никита Багацкий <bagazk2015@gmail.com>
2020-08-27 15:58:38 +0300

    Добавьте файлы проекта.

---
+++ LangsApi/Controllers/LangsController.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LangsApi.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LangsApi.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class LangsController : ControllerBase
+    {
+        private readonly List<Lang> _langs;
+
+        public LangsController(List<Lang> langs)
+        {
+            _langs = langs;
+        }
+        
+        // GET: langs
+        [HttpGet]
+        public IEnumerable<Lang> Get()
+        {
+            return _langs;
+        }
+        // GET langs/cpp
+        [HttpGet("{id}")]
+        public ActionResult<Lang> Get(string id)
+        {
+            var lang = _langs.SingleOrDefault(l => l.Id == id);
+            if (lang == null)
+                return NotFound();
+            return lang;
+        }
+        // POST: langs
+        [HttpPost]
+        public ActionResult Post([FromForm] Lang lang)
+        {
+            if (ModelState.IsValid)
+            {
+                _langs.Add(lang);
+                return Ok();
+            }
+            return BadRequest(ModelState);
+        }
+        // PUT: langs/5
+        [HttpPut("{id}")]
+        public ActionResult Put([FromForm] Lang lang)
+        {
+            var oldLang = _langs.SingleOrDefault(l => l.Id == lang.Id);
+            if (oldLang == null)
+                return NotFound();
+            if (ModelState.IsValid)
+            {
+         
+                oldLang.Year = lang.Year;
+                return Ok();
+            }
+            return BadRequest(ModelState);
+        }
+        [HttpDelete("{id}")]
+        public ActionResult Delete(string id)
+        {
+            var Lang = _langs.SingleOrDefault(l => l.Id == id);
+            if (Lang == null)
+            {
+                return NotFound();
+            }
+            _langs.Remove(Lang);
+
+            return Ok(_langs);
+        }
+    }
+}
---
+++ WebClientA/Lang.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebClientA
+{
+    public class Lang
+    {
+        public string Id { set; get; }
+        public int Year { set; get; }
+    }
+}
